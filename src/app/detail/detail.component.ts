cb6f2510f321fcefd58ab739c7658642e57a3721
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-30 20:29:28 +0100

    updated the detailed page

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -21,46 +21,72 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     </section>
 
     <section class="listing-features">
-      <h2 class="section-heading">About this housing location</h2>
-      <ul>
-        <li>Units available: {{housingLocation?.availableUnits}}</li>
-        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
-        <li>How much does it cost: {{housingLocation?.price | currency:'USD':true}}</li>
-      </ul>
+      <h2 class="section-heading">About this Housing Location</h2>
+      <div class="features-list">
+        <div class="feature-item">
+          <h3 class="feature-title">Units Available:</h3>
+          <p class="feature-value">{{ housingLocation?.availableUnits }}</p>
+        </div>
+        <div class="feature-item">
+          <h3 class="feature-title">Wifi Availability:</h3>
+          <p class="feature-value">{{ housingLocation?.wifi }}</p>
+        </div>
+        <div class="feature-item">
+          <h3 class="feature-title">Laundry Availability:</h3>
+          <p class="feature-value">{{ housingLocation?.laundry }}</p>
+        </div>
+        <div class="feature-item">
+          <h3 class="feature-title">Price:</h3>
+          <p class="feature-value">{{ housingLocation?.price | currency:'USD':true }}</p>
+        </div>
+      </div>
     </section>
 
     <section class="listing-full-description">
+      <h2 class="section-heading">Full Description</h2>
       <p>{{ housingLocation?.description }}</p>
     </section>
-    
-    <section class="listing-apply">
-      <h2 class="section-heading">Apply to live here</h2>
-      <form [formGroup]="applyForm" (submit)="submitApplication()">
-        <h3>Application Form</h3>
-        <div class="form-group form-group-inline">
-          <label for="full-name">Full name</label>
-          <input id="full-name" type="text" formControlName="fullName" />
-        </div>
-        
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input id="email" type="email" formControlName="email" />
+
+    <section class="apply-container">
+      <h2 class="apply-heading">Apply to Live Here</h2>
+
+      <div class="listing-apply">
+        <div class="apply-image">
+          <img src="/assets/travel.avif" alt="Apply Image" /> 
         </div>
 
-        <div class="form-group">
-          <label for="message">Message</label>
-          <textarea id="message" formControlName="message"></textarea>
-        </div>
+        <div class="apply-form">
+          <form [formGroup]="applyForm" (submit)="submitApplication()">
+            <h3 class="form-heading">Application Form</h3>
 
-        <div class="form-group">
-          <label for="terms">I agree to the terms and conditions</label>
-          <input id="terms" type="checkbox" />
-        </div>
+            <div class="form-group">
+              <label for="full-name">Full Name</label>
+              <input id="full-name" type="text" formControlName="fullName" class="form-input" />
+            </div>
 
-        <button class="primary" type="submit">Submit Application</button>
-      </form>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input id="email" type="email" formControlName="email" class="form-input" />
+            </div>
+
+            <div class="form-group">
+              <label for="message">Message</label>
+              <textarea id="message" formControlName="message" class="form-input form-input-textarea"></textarea>
+            </div>
+
+            <div class="form-group">
+              <label for="terms" class="terms-label">
+                <input id="terms" type="checkbox" class="terms-checkbox" />
+                <span class="agreement"> I agree to the <a href="/terms" target="_blank" rel="noopener noreferrer">terms </a> and <a href="/conditions" target="_blank" rel="noopener noreferrer">conditions</a></span>
+              </label>
+            </div>
+
+            <button class="submit-button" type="submit">Submit Application</button>
+          </form>
+        </div>
+      </div>
     </section>
+
   </article>
 
   `,
