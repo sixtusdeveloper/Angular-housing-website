b563d684a645898d59a78b369b0000403f240edf
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-30 04:20:07 +0100

    updated the Home page

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -11,6 +11,9 @@ import { RouterModule } from '@angular/router';
     <section class="listing">
       <img class="listing-photo" alt="Exterior photo of {{housingLocation.name}}" [src]="housingLocation.photo"/>
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-description">
+        {{ housingLocation.description | slice:0:80 }}{{ housingLocation.description.length > 100 ? '...' : '' }}
+      </p>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
       <a [routerLink]="['/detail', housingLocation.id]">View Details</a>
     </section>
